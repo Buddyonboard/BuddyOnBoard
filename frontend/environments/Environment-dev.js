953415ecb5757439df7adc69d57088489139f747
Tarun Bharadwaj <tarunbharadwaj25@gmail.com>
2025-08-15 22:16:52 +0530

    Updated environment.js file in frontend

--- frontend/environments/Environment-dev.js
+++ frontend/environments/Environment-dev.js
@@ -1,4 +1,4 @@
-// const API_URL = 'http://localhost:5000/api/v1';
-const API_URL = 'https://buddy-on-board.onrender.com/api/v1';
+// const API_URL = 'http://localhost:5000/api/v1'; // DEV URL
+const API_URL = 'https://buddy-on-board.onrender.com/api/v1'; // PROD URL
 
 export default API_URL;
